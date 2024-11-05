29ff70943270dc5f798c83f17a591ff295f24f29
edotheepic <86707396+edotheepic@users.noreply.github.com>
2024-11-05 22:40:01 +0700

    Add Blink functionality

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react"
+
 export default function Name(params) {
     
+    const [cursorVis, setCursorVis] = useState(true);
+
+    useEffect(() => {
+        const id = setInterval(() => {
+            setCursorVis(!cursorVis);
+        }, 1000);
+        return () => clearInterval(id);
+    });
+
     return(
-        <div>
-            Hello,
+        <div className="pt-10">
+            <div className="text-6xl">
+                Hello, I am
+            </div>
+            <div>
+
+            </div>
+
+            <div className="flex">
+                <div className="text-6xl">
+                    Edo
+                </div>
+                {cursorVis && 
+                    <div className="text-6xl">
+                        _
+                    </div>
+                }
+            </div>
         </div>
     )
 };
