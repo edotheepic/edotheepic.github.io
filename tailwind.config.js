cc3731ed5fdb3eeb25604d37f4a58d8709529871
edotheepic <86707396+edotheepic@users.noreply.github.com>
2024-11-05 22:40:28 +0700

    install gh pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,9 +18,21 @@ export default {
             transform: "translateY(0rem)",
           },
         },
+        blink: {
+          "0%": {
+            opacity: "0",
+          },
+          "50%": {
+            opacity: "1",
+          },
+          "100%": {
+            opacity: "0",
+          },
+        }
       },
       animation: {
-        float: "float 5s ease-in-out infinite"
+        float: "float 5s ease-in-out infinite",
+        blink: "blink 1s infinite"
       },
     },
   },
